ed826c42159cfbb9c4b8b6341f609922e65f46a0
ke-Grandet <326053679@qq.com>
2020-07-31 08:26:50 +0800

    hel

---
+++ docs/precache-manifest.26f685a1d83ca7da59e11f61033b8abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad5cd6e836d66d02ad67438c6e64301",
+    "url": "/pokemon-typedex/index.html"
+  },
+  {
+    "revision": "475b8afdb84fb7cf9344",
+    "url": "/pokemon-typedex/static/css/main.4e9be299.chunk.css"
+  },
+  {
+    "revision": "6e6e0b30e0d90eb00d45",
+    "url": "/pokemon-typedex/static/js/2.190590c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokemon-typedex/static/js/2.190590c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "475b8afdb84fb7cf9344",
+    "url": "/pokemon-typedex/static/js/main.131534af.chunk.js"
+  },
+  {
+    "revision": "5db708021a1567262a2f",
+    "url": "/pokemon-typedex/static/js/runtime-main.fdac7f26.js"
+  }
+]);
